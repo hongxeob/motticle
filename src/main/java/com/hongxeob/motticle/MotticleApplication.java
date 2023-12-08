79658cfa16bcbc5f04d2806d157b8ab8ec113847
Hongseob Lee <sjun2918@naver.com>
2023-12-08 14:48:03 +0900

    chore : project setting

---
+++ src/main/java/com/hongxeob/motticle/MotticleApplication.java
@@ -0,0 +1,13 @@
+package com.hongxeob.motticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongxeob/motticle/MotticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongxeob.motticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
