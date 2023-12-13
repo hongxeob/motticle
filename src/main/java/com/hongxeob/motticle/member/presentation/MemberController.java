6a1a51880b7296e5d4711ec460ddab0dfed4f17b
Hongseob Lee <sjun2918@naver.com>
2023-12-13 18:10:34 +0900

    feat : 사이트 이용을 위한 회원 정보 등록 Controller 구성

---
+++ src/main/java/com/hongxeob/motticle/member/presentation/MemberController.java
@@ -0,0 +1,29 @@
+package com.hongxeob.motticle.member.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hongxeob.motticle.member.application.MemberService;
+import com.hongxeob.motticle.member.application.dto.req.MemberInfoReq;
+import com.hongxeob.motticle.member.application.dto.res.MemberInfoRes;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<MemberInfoRes> registerInfo(@PathVariable Long id, @RequestBody MemberInfoReq req) {
+		MemberInfoRes memberInfoRes = memberService.registerInfo(id, req);
+
+		return ResponseEntity.ok(memberInfoRes);
+	}
+}
