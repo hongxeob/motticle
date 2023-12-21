b03e717c68ab9fed09081983505022ff31eff06a
Hongseob Lee <sjun2918@naver.com>
2023-12-21 19:12:43 +0900

    refactor : 아티클 레파지토리 커스텀 쿼리 추가

--- src/main/java/com/hongxeob/motticle/article/domain/ArticleRepository.java
+++ src/main/java/com/hongxeob/motticle/article/domain/ArticleRepository.java
@@ -1,6 +1,10 @@
 package com.hongxeob.motticle.article.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+	Optional<Article> findByMemberIdAndId(Long memberId, Long id);
 }
