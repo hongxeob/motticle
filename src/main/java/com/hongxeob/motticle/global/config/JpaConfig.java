0cb02e2344476198fd2efc773549300ef5c8fe05
Hongseob Lee <sjun2918@naver.com>
2023-12-22 21:57:50 +0900

    feat : QueryDSL용 config 추가

--- src/main/java/com/hongxeob/motticle/global/config/JpaConfig.java
+++ src/main/java/com/hongxeob/motticle/global/config/JpaConfig.java
@@ -3,8 +3,11 @@ package com.hongxeob.motticle.global.config;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Configuration
 @EnableJpaAuditing
@@ -12,4 +15,9 @@ public class JpaConfig {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory queryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
 }
