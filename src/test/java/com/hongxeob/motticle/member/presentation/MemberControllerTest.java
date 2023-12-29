09a0d064e1cec6e73062dd052971ba44a3741057
Hongseob Lee <sjun2918@naver.com>
2023-12-29 18:57:39 +0900

    test: 멤버 프로필 이미지 추가/수정/삭제 테스트 코드 및 RestDocs 작성

--- src/test/java/com/hongxeob/motticle/member/presentation/MemberControllerTest.java
+++ src/test/java/com/hongxeob/motticle/member/presentation/MemberControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -17,17 +18,23 @@ import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
 import com.hongxeob.motticle.global.ControllerTestSupport;
 import com.hongxeob.motticle.global.error.ErrorCode;
 import com.hongxeob.motticle.global.error.exception.BusinessException;
+import com.hongxeob.motticle.image.application.dto.res.ImagesRes;
 import com.hongxeob.motticle.member.application.dto.req.MemberInfoReq;
 import com.hongxeob.motticle.member.application.dto.req.MemberModifyReq;
 import com.hongxeob.motticle.member.application.dto.res.MemberInfoRes;
@@ -41,17 +48,27 @@ class MemberControllerTest extends ControllerTestSupport {
 	void addMemberInfoSuccessTest() throws Exception {
 
 		//given
+		MemberInfoRes memberInfoRes = new MemberInfoRes(1L, "h123@naver.com", "호빵", GenderType.FEMALE.name(), Role.USER.name(), "image.path");
 		MemberInfoReq memberInfoReq = new MemberInfoReq("호빵", GenderType.FEMALE.name());
-		MemberInfoRes memberInfoRes = new MemberInfoRes(1L, "h123@naver.com", "호빵", GenderType.FEMALE.name(), Role.USER.name());
 
-		given(memberService.registerInfo(any(), any()))
+		MockMultipartFile file = new MockMultipartFile("image", "test.jpg", "image/jpeg", "file".getBytes());
+		MockMultipartFile request = new MockMultipartFile("memberInfoReq", "memberInfoReq",
+			"application/json",
+			objectMapper.writeValueAsString(memberInfoReq).getBytes());
+
+		given(memberService.registerInfo(any(), any(), any()))
 			.willReturn(memberInfoRes);
 
-		//when -> then
-		mockMvc.perform(RestDocumentationRequestBuilders.patch("/api/members")
+		mockMvc.perform(RestDocumentationRequestBuilders.multipart("/api/members")
+				.file(file)
+				.file(request)
+				.with(updateRequest -> {
+					updateRequest.setMethod("PATCH");
+					return updateRequest;
+				})
 				.with(csrf().asHeader())
 				.header(HttpHeaders.AUTHORIZATION, "{AccessToken}")
-				.contentType(APPLICATION_JSON)
+				.contentType(MULTIPART_FORM_DATA)
 				.content(objectMapper.writeValueAsString(memberInfoReq)))
 			.andExpect(status().isOk())
 			.andDo(document("member/add-info-success",
@@ -59,6 +76,10 @@ class MemberControllerTest extends ControllerTestSupport {
 				preprocessResponse(prettyPrint()),
 				requestHeaders(
 					headerWithName("Authorization").description("Access Token")
+				),
+				requestParts(
+					partWithName("image").description("이미지"),
+					partWithName("memberInfoReq").description("멤버 추가 정보 등록 내용")
 				),
 				requestFields(
 					fieldWithPath("nickname").type(STRING).description("닉네임"),
@@ -69,7 +90,8 @@ class MemberControllerTest extends ControllerTestSupport {
 					fieldWithPath("email").type(STRING).description("이메일"),
 					fieldWithPath("nickname").type(STRING).description("닉네임"),
 					fieldWithPath("genderType").type(STRING).description("성별"),
-					fieldWithPath("role").type(STRING).description("권한")
+					fieldWithPath("role").type(STRING).description("권한"),
+					fieldWithPath("image").type(STRING).description("이미지 경로")
 				)
 			));
 	}
@@ -81,14 +103,25 @@ class MemberControllerTest extends ControllerTestSupport {
 		//given
 		MemberInfoReq memberInfoReq = new MemberInfoReq("", GenderType.FEMALE.name());
 
-		given(memberService.registerInfo(any(), any()))
+		MockMultipartFile file = new MockMultipartFile("image", "test.jpg", "image/jpeg", "file".getBytes());
+		MockMultipartFile request = new MockMultipartFile("memberInfoReq", "memberInfoReq",
+			"application/json",
+			objectMapper.writeValueAsString(memberInfoReq).getBytes());
+
+		given(memberService.registerInfo(any(), any(), any()))
 			.willThrow(new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
 
 		//when -> then
-		mockMvc.perform(RestDocumentationRequestBuilders.patch("/api/members")
+		mockMvc.perform(RestDocumentationRequestBuilders.multipart("/api/members")
+				.file(file)
+				.file(request)
+				.with(updateRequest -> {
+					updateRequest.setMethod("PATCH");
+					return updateRequest;
+				})
 				.with(csrf().asHeader())
 				.header(HttpHeaders.AUTHORIZATION, "{AccessToken}")
-				.contentType(APPLICATION_JSON)
+				.contentType(MULTIPART_FORM_DATA)
 				.content(objectMapper.writeValueAsString(memberInfoReq)))
 			.andExpect(status().isBadRequest())
 			.andDo(document("member/add-info-fail-blank-info",
@@ -96,6 +129,10 @@ class MemberControllerTest extends ControllerTestSupport {
 				preprocessResponse(prettyPrint()),
 				requestHeaders(
 					headerWithName("Authorization").description("Access Token")
+				),
+				requestParts(
+					partWithName("image").description("이미지"),
+					partWithName("memberInfoReq").description("멤버 추가 정보 등록 내용")
 				),
 				requestFields(
 					fieldWithPath("nickname").type(STRING).description("닉네임"),
@@ -118,7 +155,7 @@ class MemberControllerTest extends ControllerTestSupport {
 	void getMemberSuccessTest() throws Exception {
 
 		//given
-		MemberInfoRes memberInfoRes = new MemberInfoRes(1L, "h123@naver.com", "호빵", GenderType.FEMALE.name(), Role.USER.name());
+		MemberInfoRes memberInfoRes = new MemberInfoRes(1L, "h123@naver.com", "호빵", GenderType.FEMALE.name(), Role.USER.name(), "image.path");
 
 		given(memberService.getInfo(any()))
 			.willReturn(memberInfoRes);
@@ -140,7 +177,8 @@ class MemberControllerTest extends ControllerTestSupport {
 					fieldWithPath("email").type(STRING).description("이메일"),
 					fieldWithPath("nickname").type(STRING).description("닉네임"),
 					fieldWithPath("genderType").type(STRING).description("성별"),
-					fieldWithPath("role").type(STRING).description("권한")
+					fieldWithPath("role").type(STRING).description("권한"),
+					fieldWithPath("image").type(STRING).description("이미지 경로")
 				)
 			));
 	}
@@ -250,6 +288,66 @@ class MemberControllerTest extends ControllerTestSupport {
 				.contentType(APPLICATION_JSON))
 			.andExpect(status().isNoContent())
 			.andDo(document("member/delete-success",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestHeaders(
+					headerWithName("Authorization").description("Access Token")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("프로필 이미지 수정 성공")
+	void updateImageSuccessTest() throws Exception {
+
+		//given
+		MockMultipartFile file = new MockMultipartFile("image", "test.jpg", "image/jpeg", "file".getBytes());
+		ImagesRes imagesRes = new ImagesRes(List.of("image/path"));
+
+		given(memberService.updateImage(any(), any()))
+			.willReturn(imagesRes);
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.fileUpload("/api/members/modify/image")
+				.file(file)
+				.with(updateRequest -> {
+					updateRequest.setMethod("PATCH");
+					return updateRequest;
+				})
+				.with(csrf().asHeader())
+				.header(HttpHeaders.AUTHORIZATION, "{AccessToken}")
+				.contentType(MULTIPART_FORM_DATA))
+			.andExpect(status().isOk())
+			.andDo(document("member/update-profile-image",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestHeaders(
+					headerWithName("Authorization").description("Access Token")
+				),
+				requestParts(
+					partWithName("image").description("이미지")
+				),
+				responseFields(
+
+					fieldWithPath("imageUrl[]").type(ARRAY).description("이미지 URL")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("멤버 프로필 이미지 삭제 성공")
+	void deleteImageSuccessTest() throws Exception {
+
+		//given
+		doNothing().when(memberService).deleteImage(any());
+
+		//when -> then
+		mockMvc.perform(RestDocumentationRequestBuilders.delete("/api/members/modify/image")
+				.with(csrf().asHeader())
+				.header(HttpHeaders.AUTHORIZATION, "{AccessToken}")
+				.contentType(APPLICATION_JSON))
+			.andExpect(status().isNoContent())
+			.andDo(document("member/delete-profile-image",
 				preprocessRequest(prettyPrint()),
 				preprocessResponse(prettyPrint()),
 				requestHeaders(
