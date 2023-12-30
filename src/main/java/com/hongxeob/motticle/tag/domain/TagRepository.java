24861ba2eeaa0379ae9432a293e2aa09f4574792
Hongseob Lee <sjun2918@naver.com>
2023-12-30 18:24:22 +0900

    refactor: 페이징 쿼리 QueryDSL 적용

--- src/main/java/com/hongxeob/motticle/tag/domain/TagRepository.java
+++ src/main/java/com/hongxeob/motticle/tag/domain/TagRepository.java
@@ -2,13 +2,9 @@ package com.hongxeob.motticle.tag.domain;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {
-
-	Slice<Tag> findAllByMemberId(Long memberId, Pageable pageable);
+public interface TagRepository extends JpaRepository<Tag, Long> ,TagRepositoryCustom{
 
 	Optional<Tag> findByMemberIdAndName(Long memberId, String name);
 }
