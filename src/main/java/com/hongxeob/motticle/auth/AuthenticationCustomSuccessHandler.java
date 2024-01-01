b8dc0b9b5d8be8bdc974ca01dd227288a8cf6d73
Hongseob Lee <sjun2918@naver.com>
2024-01-01 16:54:39 +0900

    refactor: Oauth2 핸들러 리팩토링

--- src/main/java/com/hongxeob/motticle/auth/AuthenticationCustomSuccessHandler.java
+++ src/main/java/com/hongxeob/motticle/auth/AuthenticationCustomSuccessHandler.java
@@ -4,7 +4,6 @@ import static com.hongxeob.motticle.auth.domain.CookieAuthorizationRequestReposi
 
 import java.io.IOException;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import javax.servlet.http.Cookie;
@@ -17,10 +16,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.hongxeob.motticle.auth.domain.CookieAuthorizationRequestRepository;
-import com.hongxeob.motticle.auth.domain.CookieUtils;
-import com.hongxeob.motticle.auth.kakao.KaKaoOauthConfig;
 import com.hongxeob.motticle.auth.token.GeneratedToken;
 import com.hongxeob.motticle.auth.token.JwtUtil;
+import com.hongxeob.motticle.global.config.KaKaoOauthProperties;
+import com.hongxeob.motticle.global.error.ErrorCode;
+import com.hongxeob.motticle.global.error.exception.EntityNotFoundException;
+import com.hongxeob.motticle.global.util.CookieUtils;
 import com.hongxeob.motticle.member.domain.Member;
 import com.hongxeob.motticle.member.domain.MemberRepository;
 
@@ -33,29 +34,34 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthenticationCustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final static String DEFAULT_PATH = "http://localhost:8080";
-	private final KaKaoOauthConfig kaKaoOauthConfig;
+	private static final String TOKEN = "accessToken";
+	private static final String REFRESH_TOKEN = "refreshToken";
+
+	private final KaKaoOauthProperties kaKaoOauthProperties;
 	private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
 	private final JwtUtil jwtUtil;
 	private final MemberRepository memberRepository;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+		Member member = findMember(authentication);
+		GeneratedToken token = jwtUtil.generatedToken(member.getEmail(), member.getRole().getKey());
 
-		String targetUrl = determineTargetUrl(request, response, authentication);
+		String targetUrl = determineTargetUrl(request, response);
 
 		if (response.isCommitted()) {
 			log.debug("Response has already been committed.");
 			return;
 		}
-		clearAuthenticationAttributes(request, response);
-		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+
+		getRedirectStrategy().sendRedirect(request, response, getRedirectUrl(member, targetUrl, token));
 	}
 
-	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(DEFAULT_PATH);
-
+	@Override
+	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response) {
 		Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
 			.map(Cookie::getValue);
+		clearAuthenticationAttributes(request, response);
 
 		if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
 			throw new RuntimeException("redirect URIs are not matched.");
@@ -63,32 +69,31 @@ public class AuthenticationCustomSuccessHandler extends SimpleUrlAuthenticationS
 
 		String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
 
-		MyOAuth2member oAuth2User = (MyOAuth2member) authentication.getPrincipal();
-		Optional<Member> optionalMember = memberRepository.findById(oAuth2User.getMemberId());
+		return targetUrl;
+	}
 
-		if (optionalMember.isPresent()) {
-			// 회원이 존재할 경우, accessToken을 생성하고 쿼리스트링에 추가
-			Member member = optionalMember.get();
-			GeneratedToken token = jwtUtil.generatedToken(member.getEmail(), getRole(oAuth2User));
-			if (optionalMember.get().getNickname() != null && optionalMember.get().getGenderType() != null) {
-				return builder
-					.path("/home")
-					.queryParam("accessToken", token.accessToken())
-					.build()
-					.encode(StandardCharsets.UTF_8)
-					.toUriString();
-			} else {
-				return builder
-					.path("/joinForm")
-					.queryParam("accessToken", token.accessToken())
-					.build()
-					.encode(StandardCharsets.UTF_8)
-					.toUriString();
-			}
-		} else {
-			// 회원이 존재하지 않을 경우, email과 provider를 쿼리스트링에 추가
+	private Member findMember(Authentication authentication) {
+		MyOAuth2member oAuth2User = (MyOAuth2member) authentication.getPrincipal();
+		Member member = memberRepository.findById(oAuth2User.getMemberId()).orElseThrow(() -> {
+			log.warn("GET:READ:NOT_FOUND_MEMBER_BY_ID : {}", oAuth2User.getMemberId());
+			return new EntityNotFoundException(ErrorCode.NOT_FOUND_MEMBER);
+		});
+
+		return member;
+	}
+
+	private String getRedirectUrl(Member member, String targetUrl, GeneratedToken token) {
+		if (member.getNickname() != null && member.getGenderType() != null) {
 			return UriComponentsBuilder.fromUriString(targetUrl)
-				.queryParam("email", oAuth2User.getEmail())
+				.path("/home")
+				.queryParam(TOKEN, token.accessToken())
+				.queryParam(REFRESH_TOKEN, token.refreshToken())
+				.build().toUriString();
+		} else {
+			return UriComponentsBuilder.fromUriString(targetUrl)
+				.path("/joinForm")
+				.queryParam(TOKEN, token.accessToken())
+				.queryParam(REFRESH_TOKEN, token.refreshToken())
 				.build().toUriString();
 		}
 	}
@@ -100,37 +105,12 @@ public class AuthenticationCustomSuccessHandler extends SimpleUrlAuthenticationS
 
 	private boolean isAuthorizedRedirectUri(String uri) {
 		URI clientRedirectUri = URI.create(uri);
-		URI authorizedUri = URI.create(kaKaoOauthConfig.redirectUri());
+		URI authorizedUri = URI.create(kaKaoOauthProperties.redirectUri());
 
 		if (authorizedUri.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
 			&& authorizedUri.getPort() == clientRedirectUri.getPort()) {
 			return true;
 		}
 		return false;
-	}
-
-	private String buildTargetUrl(String email, MyOAuth2member oAuth2User) {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://localhost:8080/loginSuccess");
-
-		Optional<Member> optionalMember = memberRepository.findById(oAuth2User.getMemberId());
-
-		if (optionalMember.isPresent()) {
-			// 회원이 존재할 경우, accessToken을 생성하고 쿼리스트링에 추가
-			Member member = optionalMember.get();
-			GeneratedToken token = jwtUtil.generatedToken(member.getEmail(), getRole(oAuth2User));
-			builder.queryParam("accessToken", token.accessToken());
-		} else {
-			// 회원이 존재하지 않을 경우, email과 provider를 쿼리스트링에 추가
-			builder.queryParam("email", email);
-		}
-
-		return builder.build().encode(StandardCharsets.UTF_8).toUriString();
-	}
-
-	private String getRole(MyOAuth2member oAuth2User) {
-		return oAuth2User.getAuthorities().stream()
-			.findFirst()
-			.orElseThrow(IllegalAccessError::new)
-			.getAuthority();
 	}
 }
