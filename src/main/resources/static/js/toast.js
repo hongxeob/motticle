72ac4bc3323bb4b16901f28cb2388063939404aa
Hongseob Lee <sjun2918@naver.com>
2024-02-09 17:08:30 +0900

    style : toast.js 추출

---
+++ src/main/resources/static/js/toast.js
@@ -0,0 +1,18 @@
+function showToast(message, isError) {
+    const toastElement = document.getElementById('toast');
+
+    if (isError) {
+        toastElement.style.backgroundColor = '#e74c3c';
+        toastElement.style.color = '#fff';
+    } else {
+        toastElement.style.backgroundColor = '#2DD197';
+        toastElement.style.color = '#fff';
+    }
+
+    toastElement.textContent = message;
+    toastElement.style.bottom = '20px';
+
+    setTimeout(() => {
+        toastElement.style.bottom = '-100px';
+    }, 3000);
+}
